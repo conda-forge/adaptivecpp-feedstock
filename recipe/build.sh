a9463f43df28d54b4d9e55f7ffdee6c3d8236d32
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-09-21 10:44:44 -0700

    Workaround for GCC 14 on AArch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,6 +20,17 @@ else
   with_rocm_backend=OFF
 fi
 
+# Workaround for GCC 14 on AArch64 expanding __arm_* keyword-attributes to [[arm::...]]
+# which breaks Clang headers (llvm/llvm-project#78691). Upstream Clang has a fix,
+# but undefining these macros at compile time avoids the token-paste error.
+# Ref: https://github.com/llvm/llvm-project/issues/78691
+#      https://github.com/llvm/llvm-project/pull/78704
+if [[ "${target_platform}" == "linux-aarch64" ]]; then
+  ACPP_ARM_ATTR_UNDEF="-U__arm_streaming -U__arm_streaming_compatible -U__arm_locally_streaming -U__arm_preserves_za -U__arm_shared_za"
+  export CXXFLAGS="${CXXFLAGS} ${ACPP_ARM_ATTR_UNDEF}"
+  export CFLAGS="${CFLAGS} ${ACPP_ARM_ATTR_UNDEF}"
+fi
+
 cmake \
   $SRC_DIR \
   ${CMAKE_ARGS} \
